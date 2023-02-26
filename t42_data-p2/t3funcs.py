ab3dc3bf9dd78ae3007e127cf1d12f5bbf82c2ae
Adam Ponzi <apdp100@live.com>
2023-02-26 18:26:13 +0100

    Add files via upload

--- t42_data-p2/t3funcs.py
+++ t42_data-p2/t3funcs.py
@@ -108,62 +108,6 @@ class spkGenTel2:
         return spks 
 
 
-class spkGenTel21:
-    
-    def __init__(self, sg1, onset, uptime, downtime, fulltime, shift, call_shift):
-        
-        self.sg1 = sg1
-        self.onset = onset
-        
-        if isinstance(uptime,(list,np.ndarray)):
-           self.uptime1 = uptime
-           self.downtime1 = downtime
-        else:
-            self.uptime1 = [uptime]*1000
-            self.downtime1 = [downtime]*1000
-            
-        self.fulltime = fulltime - onset
-        self.shift = shift
-        self.call_shift = call_shift
-        self.call_shift_time = 0
-        self.soffsetstart = -10*(sum(self.uptime1)/len(self.uptime1) 
-                                     + sum(self.downtime1)/len(self.downtime1))
-        
-        
-    def call(self):
-        spktimes = self.sg1.call()
-        spks = np.array([])
-        tcount = 0
-    
-     
-        soffset = self.shift + self.call_shift_time
-        self.call_shift_time += self.call_shift
-        stime = self.soffsetstart + soffset
-        etime = stime + self.uptime1[tcount]
-        while etime<self.fulltime:
-            if etime > 0:
-                if stime > 0:
-                   mask = np.logical_and(spktimes>=stime, spktimes<etime)
-                else:
-                   mask = np.logical_and(spktimes>=0, spktimes<etime)
-                spks = np.append(spks,spktimes[mask])
-            stime = etime + self.downtime1[tcount]
-            tcount += 1
-            etime = stime + self.uptime1[tcount]
-        else:
-            if stime<self.fulltime:
-                if stime > 0:
-                   mask = np.logical_and(spktimes>=stime, spktimes<self.fulltime)
-                else:
-                   mask = np.logical_and(spktimes>=0, spktimes<self.fulltime) 
-                spks = np.append(spks,spktimes[mask])
-        
-    
-        spks = spks + self.onset
-        return spks 
-
-
-
 
 class ArtifPop:
     
